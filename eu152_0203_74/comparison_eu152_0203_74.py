1f262315081849b42a01f521efc90f3283f56e28
aa13xx <130700198+aa13xx@users.noreply.github.com>
2024-08-28 00:34:39 +0100

    v1.5.10

--- eu152_0203_74/comparison_eu152_0203_74.py
+++ eu152_0203_74/comparison_eu152_0203_74.py
@@ -21,8 +21,8 @@ plt.figure(2)
 #plt.vlines(x=1408.013, color="red", ls =':', label="1408 kev", ymin = 0, ymax=1e6)
 
 plt.semilogy(df_openmc.energy, df_openmc.intensity, label="original simulation", color="black", alpha=0.3)
-#plt.semilogy(df_data.energy, df_data.intensity, label="experiment", color="steelblue", alpha=0.9)
-plt.semilogy(df_openmc.energy, renorm_broadened_spectrum, label="processed simulation", color="crimson", alpha=0.5)
+plt.semilogy(df_data.energy, df_data.intensity, label="experiment", color="steelblue", alpha=0.9)
+#plt.semilogy(df_openmc.energy, renorm_broadened_spectrum, label="processed simulation", color="crimson", alpha=0.5)
 
 plt.legend()
 #plt.xlim(955,972)
@@ -53,7 +53,7 @@ peak_left_openmc = peakleft(df_openmc, est_peak_left)
 peak_right_openmc = peakright(df_openmc, est_peak_right)
 
 plt.figure(3)
-'''
+
 plt.semilogy(df_data.energy, df_data.intensity, alpha=0.9, label="experiment", color="steelblue")
 plt.vlines(x=peak_left_data, color="steelblue", alpha= 1, ls =':', ymin = 0, ymax=1e6)
 plt.vlines(x=peak_right_data, color="steelblue", alpha= 1, ls =':', ymin = 0, ymax=1e6)
@@ -61,7 +61,7 @@ plt.hlines(y=baseline_val_data, color="steelblue", alpha= 0.5, ls =':', xmin = p
 plt.fill_between(x = df_data.energy, y1 = df_data.intensity, y2 = baseline_val_data,
         where = (peak_left_data <= df_data.energy)&(df_data.energy <= peak_right_data),
         color = "steelblue", alpha = 0.1)
-'''
+
 
 plt.semilogy(df_openmc.energy, df_openmc.intensity, alpha=0.3, label="original simulation", color="black")
 plt.vlines(x=peak_left_openmc, color="crimson", alpha= 1, ls =':', ymin = 0, ymax=1e6)
@@ -70,7 +70,7 @@ plt.hlines(y=baseline_val_openmc, color="crimson", alpha= 0.5, ls =':', xmin = p
 plt.fill_between(x = df_openmc.energy, y1 = df_openmc.intensity, y2 = baseline_val_openmc,
         where = (peak_left_openmc <= df_openmc.energy)&(df_openmc.energy <= peak_right_openmc),
         color = "black", alpha = 0.1)
-
+'''
 plt.semilogy(df_openmc.energy, renorm_broadened_spectrum, alpha=0.5, label="processed simulation", color="crimson")
 #plt.vlines(x=peak_left_openmc, color="crimson", alpha= 1, ls =':', ymin = 0, ymax=1e6)
 #plt.vlines(x=peak_right_openmc, color="crimson", alpha= 1, ls =':', ymin = 0, ymax=1e6)
@@ -78,7 +78,7 @@ plt.hlines(y=baseline_val_openmc, color="crimson", alpha= 0.5, ls =':', xmin = p
 plt.fill_between(x = df_openmc.energy, y1 = renorm_broadened_spectrum, y2 = baseline_val_openmc,
         where = (peak_left_openmc <= df_openmc.energy)&(df_openmc.energy <= peak_right_openmc),
         color = "crimson", alpha = 0.1)
-
+'''
 plt.xlim(peak_left_win_data, peak_right_win_data)
 plt.ylim(1e1,1e5)
 plt.xlabel('Energy [keV]')
@@ -87,8 +87,8 @@ legend_elements = [#Patch(facecolor='steelblue', alpha = 1, label='data'),
                   #Patch(facecolor='steelblue', alpha = 0.2, label='background $=' + str(sci_notation(baseline_val_data,3)) + '$'),
                    #Patch(facecolor='crimson', alpha = 0.2, label='background $=' + str(sci_notation(baseline_val_openmc,3)) + '$'),
                    #Patch(facecolor='steelblue', alpha = 1, label='peak range'),
-                   Patch(facecolor='black', alpha = 0.3, label = "original simulation"),#, label='peak area $=' + str(sci_notation(peak_sum_val_data,3)) + '$'),
-                   Patch(facecolor='crimson', alpha = 0.3, label = "processed simulation")]#, label='peak area $=' + str(sci_notation(peak_sum_val_openmc,3)) + '$'),]
+                   Patch(facecolor='steelblue', alpha = 0.3, label = "experiment $=" + str(sci_notation(peak_sum_val_data,3)) + '$'),
+                   Patch(facecolor='crimson', alpha = 0.3, label = "processed simulation$ =" + str(sci_notation(peak_sum_val_openmc,3)) + '$'),]
 plt.legend(handles = legend_elements, loc='upper right', prop={'size': 10})
 #plt.title(f"({est_peak_left} keV,{est_peak_right} keV)")
 #plt.grid(True)
